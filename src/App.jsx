cadfe44b51fb487d913e8ef3dcc1af1d294a6c1a
CodeBustler <codebustler360online@gmail.com>
2023-11-23 15:40:11 +0530

    PROJECT FINISHED

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import WeatherApp from "./components/WeatherApp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>Weather App</h1>
+      <WeatherApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
